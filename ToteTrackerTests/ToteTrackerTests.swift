ad53ac824b63adddb26ce7ecbf4663a69655d0c9
Edward Houser <edward.houser@gmail.com>
2026-02-05 17:18:14 -0800

    Initial Commit

---
+++ ToteTrackerTests/ToteTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToteTrackerTests.swift
+//  ToteTrackerTests
+//
+//  Created by Edward Houser on 2/5/26.
+//
+
+import Testing
+@testable import ToteTracker
+
+struct ToteTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
